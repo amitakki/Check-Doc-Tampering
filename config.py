4f3997059405665a0f06c281ece46e1e23cb71a8
Amit Akki <akkiamit23@gmail.com>
2024-07-31 15:54:32 +0530

    First Commit

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+from os import environ
+
+class Config(object):
+    DEBUG = False
+    Testing = False
+
+    basedir = os.path.abspath(os.path.dirname(__file__))
+
+    SECRET_KEY = 'pianalytix'
+
+    UPLOADS = "/home/username/app/app/uploads"
+
+    SESSION_COOKIE_SECURE = True
+    DEFAULT_THEME = None
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SESSION_COOKIE_SECURE = False
+    
+
+class DebugConfig(Config):
+    DEBUG = False
+
